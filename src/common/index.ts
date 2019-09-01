3eba2c948736e760a956087199dcedabd91e2fa1
dteiml <dteiml@gmail.com>
2019-09-01 18:22:16 +0200

    lean setup

---
+++ src/common/index.ts
@@ -0,0 +1,13 @@
+import * as R from 'ramda'
+import * as S from './helpers'
+import path from 'path'
+import fs from 'fs'
+import util from 'util'
+import assert from 'assert'
+
+import propositionalLogic from '../propositional-logic'
+
+export { 
+  R, S, path, fs, util, assert,
+  propositionalLogic as logic,
+}
