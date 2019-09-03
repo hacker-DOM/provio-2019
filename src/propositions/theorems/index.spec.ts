62c5c9161237689deac06237c2d4bdad18db69a8
dteiml <dteiml@gmail.com>
2019-09-03 19:42:40 +0200

    setup dynamic tests

--- src/propositions/theorems/index.spec.ts
+++ src/propositions/theorems/index.spec.ts
@@ -3,15 +3,33 @@
 import {assert,S,R} from '../../common'
 import * as B from '../../basic/types'
 import * as A from '../axioms'
-import Theorem from './1'
+import registerRequireContextHook from 'babel-plugin-require-context-hook/register'
+registerRequireContextHook()
 
-describe (`Thereoms`, function () {
-  describe (`A == A`, function () {
-    it (`should return the proposition`, function () {
+describe (`Theorems`, function () {
+  // const importAll = (r) => {
+  //   r.keys().forEach(r)
+  // }
+  const r = require.context(`.`, false, /\d+.js$/)
+  
+  const cache = {}
+  
+  importAll(r)
+  function importAll (r) {
+    r.keys().forEach(key => cache[key] = r(key));
+  }
+
+  cache |> console.log
+
+  // Object.keys(require('module')._cache) |> console.log
+
+  R.mapObjIndexed (({default: val}, key) => {
+    it (`verifying proof of ${key}`, function () {
       const state = A._state()
-      Theorem.proof(state)
+      val.proof(state)
       const x = B._var()
-      assert.deepEqual (state.proposition(x), Theorem.proposition(x))
+      assert.deepEqual (state.proposition(x), val.proposition(x))
     })
-  })
+  }) (cache)
+  
 })
