393c2c082637d58ea93be8f66f01bee80ed1b889
dteiml <dteiml@gmail.com>
2019-09-05 17:53:19 +0200

    formatting fix

--- src/propositions/theorems/2.ts
+++ src/propositions/theorems/2.ts
@@ -7,14 +7,14 @@ const NAME = `From x and y deduce x & y`
 
 const proposition = x => x >> x
 
-const proof = ({ addVar, useAxiom, MP, QED }: T._State) => {
+const proof = ({addVar, useAxiom, MP, QED}: T._State) => {
   const x = addVar()
-  const h1_1 = useAxiom(`H1`)(x, x >> x)
-  const h2_1 = useAxiom(`H2`)(x, x >> x, x)
-  const mp_1 = MP(h2_1, h1_1)
-  const h1_2 = useAxiom(`H1`)(x, x)
-  const mp_2 = MP(mp_1, h1_2)
+  const h1_1 = useAxiom (`H1`) (x, x >> x)
+  const h2_1 = useAxiom (`H2`) (x, x >> x, x)
+  const mp_1 = MP (h2_1, h1_1)
+  const h1_2 = useAxiom (`H1`) (x, x)
+  const mp_2 = MP (mp_1, h1_2)
   QED()
 }
 
-export default B._theorem(NAME)(proposition)(proof)
+export default B._theorem (NAME) (proposition) (proof)
