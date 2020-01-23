ea759dceb9a2d0111634b92dfc7cfb36e40d4098
dteiml <dteiml@gmail.com>
2020-01-23 20:08:40 +0100

    add working steps

--- src/2-sets/theorems/empty-set-unique.ts
+++ src/2-sets/theorems/empty-set-unique.ts
@@ -15,10 +15,21 @@ proof = () => {
   const
 
   zf2 = ZF2 (),
+  // zf2 = exists (isEmpty)
+  // zf2 = !(x => !isEmpty (x))
   // TODO: Prove next line
   _isUnique = isUnique (isEmpty),
+  // _isUnique = (x, y) => ((isEmpty (x) & isEmpty (y)) >> (x === y))
   c_1 = () => theorems.conjunction.introduction (zf2, _isUnique),
-  mp_1 = inferences.ModusPonens ({implication: c_1, proposition: zf2}),
+  // c_1 = () => x >> (y >> (x & y))
+  // c_1 = () => (zf2) >> (_isUnique >> (zf2 & _isUnique))
+  mp_1 = () => inferences.ModusPonens ({implication: c_1, proposition: zf2}),
+  /*
+    mp_1 = {
+      left:
+      right:
+    }
+  */
   mp_2 = inferences.ModusPonens ({implication: mp_1, proposition: () => _isUnique})
   return mp_2
 },
