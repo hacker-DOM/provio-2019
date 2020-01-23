1dda6b41dde3eac43626a1eae91dff832b15cbfb
dteiml <dteiml@gmail.com>
2020-01-23 19:42:33 +0100

    Nand no longer a Proposition

--- src/1-predicates/primitives/nand.ts
+++ src/1-predicates/primitives/nand.ts
@@ -1,10 +1,8 @@
 import {R} from 'common'
 import {Proposition} from './proposition'
 
-export class Nand extends Proposition {
-  constructor (public left, public right) {
-    super ()
-  }
+export class Nand {
+  constructor (public left, public right) {}
 }
 
 export const nand = R.construct (Nand)
